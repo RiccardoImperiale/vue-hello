0422bdea94e2912edfb702269a13eebfb0a68e7a
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-11 11:26:14 +0100

    add random image

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Random Image!',
+            image: 'https://picsum.photos/800/600'
         }
     }
 }).mount('#app')
